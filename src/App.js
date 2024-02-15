bab52285f5d2fcd22b1e847c9f3b134d51489464
Kai Vandivier <kpvandivier@gmail.com>
2024-02-15 17:10:11 +0100

    feat: routing, remove chaff, hide header bar in legacy client apps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useConfig } from '@dhis2/app-runtime'
 // eslint-disable-next-line import/no-unresolved
 import { Plugin } from '@dhis2/app-runtime/experimental'
+import { HeaderBar } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
 import {
@@ -13,19 +14,27 @@ import {
 } from 'react-router-dom'
 import styles from './App.module.css'
 
+// Doesn't work on maintenance app
+// Doesn't work cross-domain
+const injectHeaderbarHidingStyles = (event) => {
+    try {
+        const iframe = event?.target || document.querySelector('iframe')
+        const doc = iframe.contentDocument
+        const styleElement = doc.createElement('style')
+        styleElement.textContent =
+            'div.app-shell-adapter > header { display: none; }'
+        doc.head.appendChild(styleElement)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 const Layout = ({ children }) => {
     return (
-        <div className={styles.container}>
-            {/* -> Header Bar could go here <- */}
-            <p>
-                <Link className={'hay'} to="/">
-                    Home
-                </Link>{' '}
-                <Link to="/app/simple-app">Simple App</Link>{' '}
-                <Link to="/app/pwa-app#asdf">PWA App</Link>
-            </p>
-            {children}
-        </div>
+        <>
+            <HeaderBar className={'global-shell-header'} />
+            <div className={styles.container}>{children}</div>
+        </>
     )
 }
 Layout.propTypes = { children: PropTypes.node }
@@ -33,12 +42,15 @@ Layout.propTypes = { children: PropTypes.node }
 // Save this so it can be used after browser URL changes
 const originalLocation = new URL(window.location.href)
 
+// todo: handle "whiffs" and fall-backs if 'app.html' isn't available
 const getPluginSource = (appName, baseUrl) => {
-    // if (process.env.NODE_ENV === 'development') {
-    //     return 'http://localhost:3001/plugin.html'
-    // }
     const absoluteBaseUrl = new URL(baseUrl, originalLocation)
-    return new URL(`./api/apps/${appName}/plugin.html`, absoluteBaseUrl).href
+
+    if (appName.startsWith('dhis-web')) {
+        return new URL(`./${appName}/`, absoluteBaseUrl).href
+    }
+
+    return new URL(`./api/apps/${appName}/app.html`, absoluteBaseUrl).href
 }
 
 const PluginLoader = () => {
@@ -47,10 +59,21 @@ const PluginLoader = () => {
     const { baseUrl } = useConfig()
 
     const pluginSource = React.useMemo(() => {
-        return getPluginSource(params.appName, baseUrl) + location.hash
+        const pluginSource =
+            params.appName === 'localApp'
+                ? 'http://localhost:3001/app.html'
+                : getPluginSource(params.appName, baseUrl)
+        return pluginSource + location.hash
     }, [params.appName, baseUrl, location.hash])
 
-    return <Plugin width={'100%'} height={'100%'} pluginSource={pluginSource} />
+    return (
+        <Plugin
+            width={'100%'}
+            height={'100%'}
+            pluginSource={pluginSource}
+            onLoad={injectHeaderbarHidingStyles}
+        />
+    )
 }
 
 // todo: also listen to navigations inside iframe (e.g. "Open this dashboard item in DV" links)
@@ -70,7 +93,10 @@ const MyApp = () => {
         <BrowserRouter basename={basename}>
             <Layout>
                 <Routes>
-                    <Route path="*" element={<button>Hi</button>}></Route>
+                    <Route
+                        path="*"
+                        element={<Link to="/app/localApp">Local App</Link>}
+                    ></Route>
                     <Route path="/app/:appName" element={<PluginLoader />} />
                 </Routes>
             </Layout>
