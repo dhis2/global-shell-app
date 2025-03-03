afc4b226c68e3499e3a1f7f1177b2719b2cb0585
Kai Vandivier <kpvandivier@gmail.com>
2025-03-03 22:50:14 +0100

    chore: update configs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    setupFilesAfterEnv: [
-        `${__dirname}/src/setup-tests.js`,
-    ],
+    setupFilesAfterEnv: [`${__dirname}/src/setup-tests.js`],
     testPathIgnorePatterns: ['/node_modules/', '/build/', '/.d2/'],
 }
