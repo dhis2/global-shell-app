65dbcdc0df08c937f1dc7f2c8dec0029c0b750cd
Kai Vandivier <kpvandivier@gmail.com>
2025-03-05 19:49:43 +0100

    test: polyfill TextEncoder

--- src/setup-tests.js
+++ src/setup-tests.js
@@ -1,5 +1,8 @@
+import { TextEncoder, TextDecoder } from 'util';
 import { configure } from '@testing-library/dom'
 import '@testing-library/jest-dom'
+
+Object.assign(global, { TextDecoder, TextEncoder });
 
 configure({
     testIdAttribute: 'data-test',
