bb14cd3219bd35601cc773d23e871bb66b796ff5
Kai Vandivier <kpvandivier@gmail.com>
2025-03-05 13:58:01 +0100

    feat: add app version and global shell version to debug info

--- src/components/header-bar/debug-info/use-debug-info.js
+++ src/components/header-bar/debug-info/use-debug-info.js
@@ -1,11 +1,16 @@
 import { useConfig } from '@dhis2/app-runtime'
+import { useHeaderBarContext } from '../header-bar-context.jsx'
 
 export const useDebugInfo = () => {
-    const { appName, appVersion, systemInfo } = useConfig()
+    const { appVersion: globalShellVersion, systemInfo } = useConfig()
+    const { clientAppName, clientAppVersion } = useHeaderBarContext()
 
     return {
-        app_name: appName || null,
-        app_version: appVersion?.full || null,
+        app_name: clientAppName || null,
+        // Unlike global shell version, which comes from config, app version
+        // comes from the API and is expected to be a string
+        app_version: clientAppVersion || null,
+        global_shell_version: globalShellVersion?.full || null,
         dhis2_version: systemInfo?.version || null,
         dhis2_revision: systemInfo?.revision || null,
     }
--- src/components/header-bar/header-bar-context.jsx
+++ src/components/header-bar/header-bar-context.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useMemo } from 'react'
 
 const headerBarContext = createContext({
     updateAvailable: false,
@@ -9,18 +9,34 @@ const headerBarContext = createContext({
 export const HeaderBarContextProvider = ({
     updateAvailable,
     onApplyAvailableUpdate,
+    clientAppName,
+    clientAppVersion,
     children,
 }) => {
+    const contextValue = useMemo(
+        () => ({
+            updateAvailable,
+            onApplyAvailableUpdate,
+            clientAppName,
+            clientAppVersion,
+        }),
+        [
+            updateAvailable,
+            onApplyAvailableUpdate,
+            clientAppName,
+            clientAppVersion,
+        ]
+    )
     return (
-        <headerBarContext.Provider
-            value={{ updateAvailable, onApplyAvailableUpdate }}
-        >
+        <headerBarContext.Provider value={contextValue}>
             {children}
         </headerBarContext.Provider>
     )
 }
 HeaderBarContextProvider.propTypes = {
     children: PropTypes.node,
+    clientAppName: PropTypes.string,
+    clientAppVersion: PropTypes.string,
     updateAvailable: PropTypes.bool,
     onApplyAvailableUpdate: PropTypes.func,
 }
