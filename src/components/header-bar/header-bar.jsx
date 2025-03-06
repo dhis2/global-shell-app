a9fa576cc0c6ac15c689fc8408c408dd5e7a3c3c
Kai Vandivier <49666798+KaiVandivier@users.noreply.github.com>
2025-03-06 09:41:33 +0100

    feat: command to fix pwa app index.html precache redirects (#23)
    * feat: pwa app index.html precache redirect workaround
    
    * fix: move action to command

--- src/components/header-bar/header-bar.jsx
+++ src/components/header-bar/header-bar.jsx
@@ -1,4 +1,5 @@
 import { useDataQuery, useConfig } from '@dhis2/app-runtime'
+import { IconWindow16 } from '@dhis2/ui'
 import { colors } from '@dhis2/ui-constants'
 import PropTypes from 'prop-types'
 import React, { useMemo } from 'react'
@@ -6,7 +7,8 @@ import { useNavigate } from 'react-router'
 import i18n from '../../locales/index.js'
 import CommandPalette from './command-palette/command-palette.jsx'
 import { CommandPaletteContextProvider } from './command-palette/context/command-palette-context.jsx'
-import { APP } from './command-palette/utils/constants.js'
+import { APP, COMMAND } from './command-palette/utils/constants.js'
+import { patchPwaApps } from './command-palette/utils/patch-pwa-apps.ts'
 import { HeaderBarContextProvider } from './header-bar-context.jsx'
 import { joinPath } from './join-path.js'
 import { Logo } from './logo.jsx'
@@ -64,7 +66,18 @@ export const HeaderBar = ({
     }, [data, baseUrl, navigate])
 
     // fetch commands
-    const commands = []
+    const commands = [
+        // todo: remove this command
+        {
+            type: COMMAND,
+            name: 'Patch blank PWA apps — Bug bash',
+            icon: <IconWindow16 color={colors.grey700} />,
+            dataTest: 'headerbar-patch-pwa-apps-action',
+            description:
+                'Fixes apps that appear blank (potentially Dashboard, Data Visualizer, Maps, Line Listing, or Data Entry (beta))',
+            action: patchPwaApps,
+        },
+    ]
 
     // fetch shortcuts
     const shortcuts = []
