0189e02b174d0034db450a6519eb17669efddf45
Kai Vandivier <kpvandivier@gmail.com>
2025-03-18 13:31:25 +0100

    fix: remove temporary patch-pwa command

--- src/components/header-bar/header-bar.jsx
+++ src/components/header-bar/header-bar.jsx
@@ -1,5 +1,4 @@
 import { useDataQuery, useConfig } from '@dhis2/app-runtime'
-import { IconWindow16 } from '@dhis2/ui'
 import { colors } from '@dhis2/ui-constants'
 import PropTypes from 'prop-types'
 import React, { useMemo } from 'react'
@@ -7,8 +6,7 @@ import { useNavigate } from 'react-router'
 import i18n from '../../locales/index.js'
 import CommandPalette from './command-palette/command-palette.jsx'
 import { CommandPaletteContextProvider } from './command-palette/context/command-palette-context.jsx'
-import { APP, COMMAND } from './command-palette/utils/constants.js'
-import { patchPwaApps } from './command-palette/utils/patch-pwa-apps.ts'
+import { APP } from './command-palette/utils/constants.js'
 import { HeaderBarContextProvider } from './header-bar-context.jsx'
 import { joinPath } from './join-path.js'
 import { Logo } from './logo.jsx'
@@ -66,18 +64,7 @@ export const HeaderBar = ({
     }, [data, baseUrl, navigate])
 
     // fetch commands
-    const commands = [
-        // todo: remove this command
-        {
-            type: COMMAND,
-            name: 'Patch blank PWA apps',
-            icon: <IconWindow16 color={colors.grey700} />,
-            dataTest: 'headerbar-patch-pwa-apps-action',
-            description:
-                'Fixes apps that appear blank (potentially Dashboard, Data Visualizer, Maps, Line Listing, or Data Entry (beta))',
-            action: patchPwaApps,
-        },
-    ]
+    const commands = []
 
     // fetch shortcuts
     const shortcuts = []
