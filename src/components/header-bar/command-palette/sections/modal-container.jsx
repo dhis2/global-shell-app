347741e7ec31bf5dfb219b257c0c871f24519728
Joseph John Aas Cooper <33054985+cooper-joe@users.noreply.github.com>
2025-03-18 09:24:49 +0100

    fix(command-palette): adjust height

--- src/components/header-bar/command-palette/sections/modal-container.jsx
+++ src/components/header-bar/command-palette/sections/modal-container.jsx
@@ -45,7 +45,7 @@ const ModalContainer = forwardRef(function ModalContainer(
                     border-radius: 1px;
                     padding: 1px;
                     width: 572px;
-                    max-height: 544px;
+                    max-height: 560px;
                     border-radius: 3px;
                     background: ${colors.white};
                     box-shadow: ${elevations.e100};
