b7e67dceffa24d1e78564963d1b4096fa79b8de2
Mozafar Haider <mozafar@dhis2.org>
2026-04-15 17:14:00 +0100

    fix(DHIS2-21312): handle invalid app URLs in v43

--- src/components/ConnectedHeaderbar.jsx
+++ src/components/ConnectedHeaderbar.jsx
@@ -75,10 +75,15 @@ export function ConnectedHeaderBar({ appsInfoQuery }) {
         if (!params.appName || !appsInfoQuery.data) {
             return
         }
+
+        const bundledApps =
+            appsInfoQuery.data.bundledApps?.apps ??
+            appsInfoQuery.data.bundledApps
+
         return getAppVersion(
             params.appName,
             appsInfoQuery.data.apps,
-            appsInfoQuery.data.bundledApps
+            bundledApps
         )
     }, [appsInfoQuery.data, params.appName])
 
