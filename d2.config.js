9125841e0a8978121dc23e35c7a7b38b5ec7d1dd
Mozafar Haider <mozafar@dhis2.org>
2026-09-24 14:21:46 +0100

    fix: support continuous release to AppHub

--- d2.config.js
+++ d2.config.js
@@ -1,7 +1,9 @@
 const config = {
+    id: '3491d347-bc0c-4eea-8412-1670607fd3f7',
     type: 'app',
     name: 'global-shell', // This must be exact to be used as the global shell
     title: 'Global Shell',
+    minDHIS2Version: '2.42',
 
     pwa: { enabled: true },
     direction: 'auto',
